7c489359749342453574332bd30093fe606bf130
Legend8-8 <Legend88.dev@gmail.com>
2026-09-08 17:11:36 +0100

    fix: allow SOG revive action remote execution

--- config/CfgRemoteExec.hpp
+++ config/CfgRemoteExec.hpp
@@ -227,6 +227,13 @@ class CfgRemoteExec
             jip = 0;
         };
 
+        // S.O.G. Advanced Revive installs/removes casualty actions globally and persists them for JIP.
+        class VN_fnc_revive_actions_local
+        {
+            allowedTargets = 0;
+            jip = 1;
+        };
+
         class bn_koth_fnc_vehicles_mobileRespawn_requestTeleport
         {
             allowedTargets = 2;
